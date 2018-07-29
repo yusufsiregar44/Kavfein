046b859be8443fc71c059dbbad0f11cbd23033ec
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-07-29 13:23:07 +0700

    configured firebase and edited app.vue

--- client-new/src/store.js
+++ client-new/src/store.js
@@ -1,7 +1,25 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import * as firebase from 'firebase';
 
-Vue.use(Vuex)
+var config = {
+  apiKey: "AIzaSyCry2RCqBRix9Jr60ly68tKYXC0mhXmNBw",
+  authDomain: "kavfein.firebaseapp.com",
+  databaseURL: "https://kavfein.firebaseio.com",
+  projectId: "kavfein",
+  storageBucket: "kavfein.appspot.com",
+  messagingSenderId: "150526720983"
+};
+firebase.initializeApp(config);
+const firestore = firebase.firestore();
+const auth = firebase.auth();
+
+const settings = {
+  timestampsInSnapshots: true
+};
+firestore.settings(settings);
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
@@ -11,6 +29,6 @@ export default new Vuex.Store({
 
   },
   actions: {
-
-  }
-})
+    
+  },
+});
